8fdd7d65bd75834896b3dff1b22bdb26030c4d69
Iyare <toolz.skillz@gmail.com>
2020-03-23 11:22:46 +0100

    refactored: formatDecimal method

--- src/main/java/net/nimoniks/util/Json2XmlTest.java
+++ src/main/java/net/nimoniks/util/Json2XmlTest.java
@@ -7,34 +7,8 @@ import javax.xml.stream.XMLStreamException;
 public class Json2XmlTest {
 
 	public static void main(String[] args) throws IOException, XMLStreamException {
-		
-		String jsonString = "  {\n" + 
-				"       \"document\":{\n" + 
-				"          \"a\":1,\n" + 
-				"          \"b\":2,\n" + 
-				"          \"c\":{\n" + 
-				"             \"d\":\"text\"\n" + 
-				"          },\n" + 
-				"          \"e\":[1, 2, 3],\n" + 
-				"          \"f\":[[1, 2, 3], [4, 5,6]],\n" + 
-				"          \"g\":null,\n" + 
-				"          \"h\":[\n" + 
-				"             {\n" + 
-				"                \"i\":true,\n" + 
-				"                \"j\":false\n" + 
-				"             }\n" + 
-				"          ],\n" + 
-				"          \"k\":[\n" + 
-				"             [\n" + 
-				"                {\"l\":1, \"m\":2}\n" + 
-				"             ],\n" + 
-				"             [\n" + 
-				"                {\"n\":3, \"o\":4},\n" + 
-				"                {\"p\":5, \"q\":6}\n" + 
-				"             ]\n" + 
-				"          ]\n" + 
-				"       }\n" + 
-				"    }";
+//	System.out.println("Hello World");
+		String jsonString = "{\"rrr\":\"190014112562\",\"channnel\":\"WEB\",\"channel\":\"WEB\",\"amount\":15000.00,\"transactiondate\":\"2017-03-14 00:00:00\",\"debitdate\":\"2017-03-14 15:03:51\",\"bank\":\"057\",\"branch\":\"\",\"serviceTypeId\":\"1509322635002\",\"payerName\":\"Ade Badmus\",\"payerPhoneNumber\":\"\",\"payerEmail\":\"\",\"type\":\"PY\",\"customFieldData\":[{\"DESCRIPTION\":\"Employer Code\",\"COLVAL\":\"12\"}],\"parentRRRDetails\":{},\"chargeFee\":157.50}";
 
 		String xml = Json2XmlUtil.convert(jsonString);
 		System.out.println("JSON:\n " + jsonString);
--- src/main/java/net/nimoniks/util/Json2XmlUtil.java
+++ src/main/java/net/nimoniks/util/Json2XmlUtil.java
@@ -3,6 +3,7 @@ package net.nimoniks.util;
 
 import java.io.InputStream;
 import java.text.DecimalFormat;
+import java.util.Iterator;
 
 import org.json.JSONObject;
 import org.json.XML;
@@ -10,7 +11,7 @@ import org.json.XML;
 public class Json2XmlUtil {
 
 	private static InputStream json;
-	private static String pattern = "#.00"; //To 2 decimal places
+	private static String pattern = "#.00"; // To 2 decimal places
 	private static DecimalFormat decimalFormat = new DecimalFormat(pattern);
 
 	public static String convert(String anyJsonString) {
@@ -18,16 +19,29 @@ public class Json2XmlUtil {
 		try {
 			JSONObject json = new JSONObject(anyJsonString.toString());
 			formatDecimals(json);
-			xml = XML.toString(json, "root"); //Set root tag
+			xml = XML.toString(json, "root"); // Set root tag
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return xml;
 	}
 
-	//Method to format json values to 2 decimals places i.e. double in this case; can be refactored for other data types
+	// Method to format json values to 2 decimals places i.e. double in this case;
+	// can be refactored for other data types
 	static void formatDecimals(JSONObject json) {
 		for (String key : json.keySet()) {
+			Object value = json.get(key);
+			if (value instanceof Double) {
+				value = decimalFormat.format(value);
+				json.put(key, value);
+			}
+		}
+	}
+
+	static void formatDecimal2(JSONObject json) throws Exception {
+		Iterator iterator = json.keys();
+		while (iterator.hasNext()) {
+			String key = (String) iterator.next();
 			Object value = json.get(key);
 			if (value instanceof Double) {
 				value = decimalFormat.format(value);
